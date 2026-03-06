6a0c20c4037f2ebe4cfb3511d5ddb6ac365ee81c
Charbel Elfeghali <elfeghac@cn-f002.server.mila.quebec>
2026-03-06 16:35:50 -0500

    fix data gen path for wiki on snow

--- scripts/data/prepare_wikipedia.sh
+++ scripts/data/prepare_wikipedia.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -eou pipefail
 
-TOPICS_FILE="/home/mila/e/elfeghac/scratch/enwiki_topics2020.csv"
-OUTPUT_DIR="/home/mila/e/elfeghac/scratch/data/datasets/wiki_bio"
+TOPICS_FILE="/work/scratch/enwiki_topics2020.csv"
+OUTPUT_DIR="/work/scratch/data/datasets/wiki_bio"
 CHUNK_SIZE=1024
 
 python -m tiered.data.prepare_wikipedia \
