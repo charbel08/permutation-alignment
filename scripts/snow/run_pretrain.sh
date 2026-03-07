a07d3daa4b4af871d347cb373126a11334c37d4d
Charbel Elfeghali <elfeghac@cn-f002.server.mila.quebec>
2026-03-06 20:24:21 -0500

    organized running scripts

--- scripts/snow/run_pretrain.sh
+++ scripts/snow/run_pretrain.sh
@@ -5,7 +5,7 @@ export TRANSFORMERS_CACHE=/work/scratch/hf
 
 cd /work/permutation-alignment
 
-python -m tiered.train.tiered_pretrain \
+torchrun --standalone --nproc_per_node=8 -m tiered.train.tiered_pretrain \
     --data_path /work/scratch/data/datasets/wiki_bio/retain \
     --output_dir /work/scratch/checkpoints/tiered_pretrain_64m \
     --key_path configs/keys/key_64m_20pct_mixed.json \
@@ -13,8 +13,7 @@ python -m tiered.train.tiered_pretrain \
     --num_heads 32 \
     --num_layers 12 \
     --context_size 1024 \
-    --batch_size 16 \
-    --grad_accum_steps 6 \
+    --batch_size 12 \
     --learning_rate 6e-4 \
     --min_lr 6e-5 \
     --max_steps 35696 \
@@ -24,4 +23,4 @@ python -m tiered.train.tiered_pretrain \
     --eval_steps 60 \
     --save_interval 1000 \
     --wandb_project tiered-alignment \
-    --run_name pretrain_64m_snow_1gpu
+    --run_name pretrain_64m_snow_8gpu
