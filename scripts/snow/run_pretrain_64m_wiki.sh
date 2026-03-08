afacc57a22f4ec77b6fae5cf2df3b67283c3857c
Charbel Elfeghali <elfeghac@cn-f002.server.mila.quebec>
2026-03-08 10:55:00 -0400

    wiki train script hyperparams

--- scripts/snow/run_pretrain_64m_wiki.sh
+++ scripts/snow/run_pretrain_64m_wiki.sh
@@ -5,7 +5,7 @@ export TRANSFORMERS_CACHE=/work/scratch/hf
 
 cd /work/permutation-alignment
 
-torchrun --standalone --nproc_per_node=8 -m tiered.train.tiered_pretrain \
+torchrun --standalone --nproc_per_node=4 -m tiered.train.tiered_pretrain \
     --data_path /work/scratch/data/datasets/wiki_bio/retain \
     --output_dir /work/scratch/checkpoints/tiered_pretrain_64m \
     --key_path configs/keys/key_64m_20pct_mixed.json \
@@ -13,11 +13,11 @@ torchrun --standalone --nproc_per_node=8 -m tiered.train.tiered_pretrain \
     --num_heads 32 \
     --num_layers 12 \
     --context_size 1024 \
-    --batch_size 12 \
-    --grad_accum_steps 2 \
+    --batch_size 24 \
+    --grad_accum_steps 1 \
     --learning_rate 6e-4 \
     --min_lr 6e-5 \
-    --max_steps 35696 \
+    --max_steps 71392 \
     --warmup_steps 500 \
     --log_interval 1 \
     --eval_interval 500 \
