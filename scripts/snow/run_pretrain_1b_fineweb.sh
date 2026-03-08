171cdd7b3740125b7d178a43c1d16504afec456f
Charbel Elfeghali <elfeghac@cn-f002.server.mila.quebec>
2026-03-08 11:47:15 -0400

    cleanup and 1b training script

---
+++ scripts/snow/run_pretrain_1b_fineweb.sh
@@ -0,0 +1,29 @@
+source /work/.bashrc
+
+export HF_HOME=/work/scratch/hf
+export TRANSFORMERS_CACHE=/work/scratch/hf
+
+cd /work/permutation-alignment
+
+torchrun --standalone --nproc_per_node=8 -m tiered.train.tiered_pretrain \
+    --data_path /work/scratch/data/datasets/fineweb/retain \
+    --output_dir /work/scratch/checkpoints/fineweb/tiered_pretrain_1b \
+    --key_path configs/keys/key_1b_10pct_mixed.json \
+    --hidden_size 2048 \
+    --intermediate_size 8192 \
+    --num_heads 16 \
+    --num_layers 16 \
+    --untie_weights \
+    --context_size 1024 \
+    --batch_size 22 \
+    --grad_accum_steps 4 \
+    --learning_rate 2.1e-4 \
+    --min_lr 2.1e-5 \
+    --max_steps 138022 \
+    --warmup_steps 1000 \
+    --log_interval 1 \
+    --eval_interval 1000 \
+    --eval_steps 60 \
+    --save_interval 10000 \
+    --wandb_project tiered-alignment-pretrain \
+    --run_name pretrain_1b_fineweb
