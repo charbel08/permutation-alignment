faf526b7b615e214d2facf91521770cd27f6f3df
Charbel Elfeghali <elfeghac@cn-f002.server.mila.quebec>
2026-03-07 19:16:38 -0500

    organized running scripts

--- src/tiered/train/tiered_pretrain.py
+++ src/tiered/train/tiered_pretrain.py
@@ -22,6 +22,7 @@ from contextlib import nullcontext
 import torch
 import torch.distributed as dist
 import torch.optim as optim
+from torch.amp import autocast
 from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.optim.lr_scheduler import CosineAnnealingLR, LinearLR, SequentialLR
 from torch.utils.data import DataLoader, DistributedSampler
@@ -131,7 +132,8 @@ def evaluate(model, dataloader, key, device, num_steps=50, is_distributed=False,
         labels = batch["labels"].to(device)
         
         # Evaluate C1
-        outputs_c1 = model(input_ids, labels=labels)
+        with autocast(device_type="cuda", dtype=torch.bfloat16):
+            outputs_c1 = model(input_ids, labels=labels)
         loss_c1 = outputs_c1.loss.item()
         preds_c1 = outputs_c1.logits[:, :-1, :].argmax(dim=-1)
         targets_c1 = labels[:, 1:]
@@ -140,7 +142,8 @@ def evaluate(model, dataloader, key, device, num_steps=50, is_distributed=False,
         
         # Evaluate C2
         apply_permutation(model, key, plan=swap_plan)
-        outputs_c2 = model(input_ids, labels=labels)
+        with autocast(device_type="cuda", dtype=torch.bfloat16):
+            outputs_c2 = model(input_ids, labels=labels)
         loss_c2 = outputs_c2.loss.item()
         preds_c2 = outputs_c2.logits[:, :-1, :].argmax(dim=-1)
         targets_c2 = labels[:, 1:]
@@ -418,8 +421,9 @@ def train(args):
             sync_ctx = nullcontext() if (not is_distributed or is_last_micro) else model.no_sync()
             
             with sync_ctx:
-                outputs_c1 = model(batch["input_ids"], labels=batch["labels"])
-                loss_c1 = outputs_c1.loss
+                with autocast(device_type="cuda", dtype=torch.bfloat16):
+                    outputs_c1 = model(batch["input_ids"], labels=batch["labels"])
+                    loss_c1 = outputs_c1.loss
                 
                 with torch.no_grad():
                     preds_c1 = outputs_c1.logits[:, :-1, :].argmax(dim=-1)
@@ -445,8 +449,9 @@ def train(args):
             sync_ctx = nullcontext() if (not is_distributed or is_last_micro) else model.no_sync()
             
             with sync_ctx:
-                outputs_c2 = model(batch["input_ids"], labels=batch["labels"])
-                loss_c2 = outputs_c2.loss
+                with autocast(device_type="cuda", dtype=torch.bfloat16):
+                    outputs_c2 = model(batch["input_ids"], labels=batch["labels"])
+                    loss_c2 = outputs_c2.loss
                 
                 with torch.no_grad():
                     preds_c2 = outputs_c2.logits[:, :-1, :].argmax(dim=-1)
